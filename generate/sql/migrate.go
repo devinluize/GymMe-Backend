7bdbc7d84f831bd701fa7e5f53393f48786f1c52
devinluize <71080055+devinluize@users.noreply.github.com>
2024-08-06 14:25:45 +0700

    Create migrate.go

---
+++ generate/sql/migrate.go
@@ -0,0 +1,49 @@
+package migration
+
+import (
+	configenv "GymMe-Backend/api/config"
+	entities "GymMe-Backend/api/entities/auth"
+	"gorm.io/driver/sqlserver"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"gorm.io/gorm/schema"
+	"log"
+	"time"
+)
+
+func Migrate() {
+	configenv.InitEnvConfigs(false, "")
+	logEntry := "Auto Migrating..."
+
+	dsn := "sqlserver://localhost:1433?database=assignmentDB&connection+timeout=30&encrypt=disable&trustServerCertificate=false&app name=SqlClient"
+	newLogger := logger.New(
+		log.New(log.Writer(), "\r\n", log.LstdFlags),
+		logger.Config{
+			SlowThreshold: time.Second,
+			LogLevel:      logger.Info,
+			Colorful:      true,
+		},
+	)
+	db, err := gorm.Open(sqlserver.Open(dsn), &gorm.Config{
+		Logger: newLogger,
+		NamingStrategy: schema.NamingStrategy{
+			SingularTable: false,
+		},
+		DisableForeignKeyConstraintWhenMigrating: false,
+	})
+
+	if err != nil {
+		log.Printf("%s Failed to connect to database with error: %s", logEntry, err)
+		panic(err)
+	}
+	err = db.AutoMigrate(
+		&entities.RegisterPayloads{},
+	)
+
+	if err != nil {
+		log.Printf("%s Failed with error: %s", logEntry, err)
+		panic(err)
+	}
+
+	log.Printf("%s Success", logEntry)
+}
